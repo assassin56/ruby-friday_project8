f3d383761e4853e7fb6a1ae8e81b9f1ed9a0f405
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-15 10:54:42 -0700

    add new test for .all method. Intentionally ensure the test fails.

--- spec/class_dictionary_spec.rb
+++ spec/class_dictionary_spec.rb
@@ -1,10 +1,16 @@
 require 'rspec'
 require 'pry'
-require 'class_Dictionary'
+require 'class_dictionary'
 require 'words'
 
 describe('#Class_Dictionary') do
   before(:each) do
     Class_Dictionary.clear()
+  end
+
+  describe('.all') do
+    it('this test should return an empty array when there are no words in the dictionary') do
+      expect(Class_Dictionary.all).to(eq(false))
+    end
   end
 end
