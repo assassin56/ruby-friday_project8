6f930c09ad4541dbe515785bd7d11cd6ca1ff7bf
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-15 14:23:30 -0700

    add test for .clear method. Ensure test fails.

--- spec/class_dictionary_spec.rb
+++ spec/class_dictionary_spec.rb
@@ -14,11 +14,16 @@ describe('#Class_Dictionary') do
     end
   end
 
-  # describe('.clear') do
-  #   it('clears the dictionary of all words') do
-      
-  #   end
-  # end
+  describe('.clear') do
+    it('clears the dictionary of all words') do
+      class_dict = Class_Dictionary.new({:name => "Consistency", :id => nil})
+      class_dict.save()
+      class_dict2 = Class_Dictionary.new({:name => "Monotany", :id => nil})
+      class_dict2.save()
+      Class_Dictionary.clear
+      expect(Class_Dictionary.all).to(eq(false))
+    end
+  end
 
   describe('#save') do
     it('updates the dictionary with a new word') do
