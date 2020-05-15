962640075ad8781043727543452e8db231d2c3d6
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-15 16:28:05 -0700

    add get request method for the home page. Troubleshooting errors on why the page is not loading.

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/')
-require('./lib/')
+require('./lib/class_dictionary')
+# require('./lib/word')
 require('pry')
 also_reload('lib/**/*.rb')
 
 get('/') do
-  
+  @dictionary = Class_Dictionary.all
+  erb(:class_dictionary)
 end
