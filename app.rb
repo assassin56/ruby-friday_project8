8cbd17e07d835de6fed55b714e431bd97bbcd834
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-17 14:51:41 -0700

    add all routeos needed for both words and their respective definitions.

--- app.rb
+++ app.rb
@@ -1,15 +1,18 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/class_dictionary')
-# require('./lib/word')
+require('./lib/class_definitions')
 require('pry')
 also_reload('lib/**/*.rb')
 
+# a test #
 
 get('/test') do
   @something = "this is a variable"
   erb(:whatever)
 end
+
+# methods for the homepage #
 
 get('/') do
   @dictionary = Class_Dictionary.all
@@ -21,7 +24,55 @@ get('/dictionary') do
   erb(:dictionary)
 end
 
+# methods pertaining to particular words #
+
 get('/word/new') do
   erb(:new_word)
 end
 
+get('/word/:id') do
+  @dictionary = Class_Dictionary.find(params[:id].to_i())
+  erb(:word)
+end
+
+patch('/word/:id') do
+  @dictionary = Class_Dictionary.find(params[:id].to_i())
+  @dictionary.update(params[:name])
+  @dictionary = Class_Dictionary.all
+  erb(:word)
+end
+
+delete('/word/:id') do
+  @dictionary = Class_Dictionary.find(params[:id].to_i())
+  @dictionary.delete()
+  @dictionary = Class_Dictionary.all
+  erb(:word)
+end
+
+get('/word/search') do
+  @dictionary = Class_Dictionary.search(params[:name])
+  erb(:search)
+end
+
+# methods pertaining to word definitions #
+
+post('/word/:id/definition') do
+  @dictionary = Class_Dictionary.find(params[:id].to_i())
+  definition = Class_Definition.find(params[:def_id].to_i())
+  definition.save()
+  erb(:defintion)
+end
+
+patch('/word/:id/defintion/:definition_id') do
+  @dictionary = Class_Dictionary.find(params[:id].to_i())
+  definition = Class_Definition.find(params[:definition_id].to_i())
+  definition.update(params[:name], @word.id)
+  erb(:word)
+end
+
+delete('/word/:id/defintion/:defintion_id') do
+  defintion = Class_Definition.find(params[:definition_id].to_i())
+  definition.delete()
+  @dicionary = Class_Dictionary.find(params[:id].to_i())
+  erb(:word)
+end
