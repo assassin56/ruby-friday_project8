bec2fa980a636eaa7284ff52b7286ce10ffa634e
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-17 15:54:10 -0700

    add new test for .clear method. Test fails while testing for false.

--- spec/class_defintions_spec.rb
+++ spec/class_defintions_spec.rb
@@ -9,6 +9,17 @@ describe('#Class_Definitions') do
 
   describe('.all') do
     it('this test should return an empty list of definitions when there are no words') do
+      expect(Class_Definitions.all).to(eq([]))
+    end
+  end
+
+  describe('.clear') do
+    it('this test should clear the dictionary of all definitions') do
+      class_def = Class_Definitions.new({:name => "conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.", :id => nil})
+      class_def.save()
+      class_def2 = Class_Definitions.new({:name => "lack of variety and interest; tedious repetition and routine.", :id => nil})
+      class_def2.save()
+      Class_Definitions.clear()
       expect(Class_Definitions.all).to(eq(false))
     end
   end
