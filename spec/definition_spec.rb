7c8e13a7bb54c165905bb4ab70c126ce97ac657a
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-27 15:22:19 -0700

    uncomment all tests from word_spec and definition_spec.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,76 +1,76 @@
-# require 'rspec'
-# require 'pry'
-# require 'definition'
+require 'rspec'
+require 'pry'
+require 'definition'
 
-# describe('#Definition') do
-#   before(:each) do
-#     Definition.clear()
-#   end
+describe('#Definition') do
+  before(:each) do
+    Definition.clear()
+  end
 
-#   describe('.all') do
-#     it('this test should return an empty list of definitions when there are no words') do
-#       expect(Definition.all).to(eq([]))
-#     end
-#   end
+  describe('.all') do
+    it('this test should return an empty list of definitions when there are no words') do
+      expect(Definition.all).to(eq([]))
+    end
+  end
 
-#   describe('.clear') do
-#     it('this test should clear the dictionary of all definitions') do
-#       definition = Definition.new({:definition => "conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.", :word_id => @word_id, :id => nil})
-#       definition.save()
-#       definition2 = Definition.new({:definition => "lack of variety and interest; tedious repetition and routine.", :word_id => @word_id, :id => nil})
-#       definition2.save()
-#       Definition.clear()
-#       expect(Definition.all).to(eq([]))
-#     end
-#   end
+  describe('.clear') do
+    it('this test should clear the dictionary of all definitions') do
+      definition = Definition.new({:definition => "conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.", :word_id => @word_id, :id => nil})
+      definition.save()
+      definition2 = Definition.new({:definition => "lack of variety and interest; tedious repetition and routine.", :word_id => @word_id, :id => nil})
+      definition2.save()
+      Definition.clear()
+      expect(Definition.all).to(eq([]))
+    end
+  end
 
-#   describe('.find') do
-#     it('this test should find a word by its id') do
-#       definition = Definition.new({:definition => "conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.",:word_id => @word_id, :id => nil})
-#       definition.save()
-#       definition2 = Definition.new({:definition => "lack of variety and interest; tedious repetition and routine.", :word_id => @word_id, :id => nil})
-#       definition2.save()
-#       expect(Definition.find(definition.id)).to(eq(definition))
-#     end
-#   end
+  describe('.find') do
+    it('this test should find a word by its id') do
+      definition = Definition.new({:definition => "conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.",:word_id => @word_id, :id => nil})
+      definition.save()
+      definition2 = Definition.new({:definition => "lack of variety and interest; tedious repetition and routine.", :word_id => @word_id, :id => nil})
+      definition2.save()
+      expect(Definition.find(definition.id)).to(eq(definition))
+    end
+  end
 
-#   describe('#update') do
-#     it('updates a definition based on its id') do
-#       definition = Definition.new({:definition => "none existent definition", :word_id => @word_id, :id => nil})
-#       definition.save()
-#       definition.update("conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.", @word_id)
-#       expect(definition.definition).to(eq("conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness."))
-#     end
-#   end
+  describe('#update') do
+    it('updates a definition based on its id') do
+      definition = Definition.new({:definition => "none existent definition", :word_id => @word_id, :id => nil})
+      definition.save()
+      definition.update("conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.", @word_id)
+      expect(definition.definition).to(eq("conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness."))
+    end
+  end
 
-#   describe('#save') do
-#     it('updates the word with a new definition.') do
-#       definition = Definition.new({:definition => "conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.", :word_id => @word_id, :id => nil})
-#       definition.save()
-#       definition2 = Definition.new({:definition => "lack of variety and interest; tedious repetition and routine.", :word_id => @word_id, :id => nil})
-#       definition2.save()
-#       expect(Definition.all).to(eq([definition, definition2]))
-#     end
-#   end
+  describe('#save') do
+    it('updates the word with a new definition.') do
+      definition = Definition.new({:definition => "conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.", :word_id => @word_id, :id => nil})
+      definition.save()
+      definition2 = Definition.new({:definition => "lack of variety and interest; tedious repetition and routine.", :word_id => @word_id, :id => nil})
+      definition2.save()
+      expect(Definition.all).to(eq([definition, definition2]))
+    end
+  end
 
-#   describe('#==') do
-#     it('checks for the same definition if it has the same attributes as another definition') do
-#       definition = Definition.new({:definition => "conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.", :word_id => @word_id, :id => nil})
-#       definition.save()
-#       definition2 = Definition.new({:definition => "conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.",:word_id => @word_id, :id => nil})
-#       definition2.save()
-#       expect(definition).to(eq(definition2))
-#     end
-#   end
+  describe('#==') do
+    it('checks for the same definition if it has the same attributes as another definition') do
+      definition = Definition.new({:definition => "conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.", :word_id => @word_id, :id => nil})
+      definition.save()
+      definition2 = Definition.new({:definition => "conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.",:word_id => @word_id, :id => nil})
+      definition2.save()
+      expect(definition).to(eq(definition2))
+    end
+  end
 
-#   describe('#delete') do
-#     it('deletes a definition by id') do
-#       definition = Definition.new({:definition => "conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.",:word_id => @word_id, :id => nil})
-#       definition.save()
-#       definition2 = Definition.new({:definition => "lack of variety and interest; tedious repetition and routine.",:word_id => @word_id, :id => nil})
-#       definition2.save()
-#       definition.delete()
-#       expect(Definition.all).to(eq([definition2]))
-#     end
-#   end
-# end
+  describe('#delete') do
+    it('deletes a definition by id') do
+      definition = Definition.new({:definition => "conformity in the application of something, typically that which is necessary for the sake of logic, accuracy, or fairness.",:word_id => @word_id, :id => nil})
+      definition.save()
+      definition2 = Definition.new({:definition => "lack of variety and interest; tedious repetition and routine.",:word_id => @word_id, :id => nil})
+      definition2.save()
+      definition.delete()
+      expect(Definition.all).to(eq([definition2]))
+    end
+  end
+end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,97 +1,97 @@
-# require 'rspec'
-# require 'pry'
-# require 'word'
-# require 'definition'
+require 'rspec'
+require 'pry'
+require 'word'
+require 'definition'
 
-# describe('#Word') do
-#   before(:each) do
-#     Word.clear()
-#   end
+describe('#Word') do
+  before(:each) do
+    Word.clear()
+  end
 
-#   describe('.all') do
-#     it('this test should return an empty array when there are no words in the dictionary') do
-#       expect(Word.all).to(eq([]))
-#     end
-#   end
+  describe('.all') do
+    it('this test should return an empty array when there are no words in the dictionary') do
+      expect(Word.all).to(eq([]))
+    end
+  end
 
-#   describe('.clear') do
-#     it('clears the dictionary of all words') do
-#       new_word = Word.new({:name => "Consistency", :id => nil})
-#       new_word.save()
-#       new_word2 = Word.new({:name => "Monotony", :id => nil})
-#       new_word2.save()
-#       Word.clear
-#       expect(Word.all).to(eq([]))
-#     end
-#   end
+  describe('.clear') do
+    it('clears the dictionary of all words') do
+      new_word = Word.new({:name => "Consistency", :id => nil})
+      new_word.save()
+      new_word2 = Word.new({:name => "Monotony", :id => nil})
+      new_word2.save()
+      Word.clear
+      expect(Word.all).to(eq([]))
+    end
+  end
 
-#   describe('.find') do
-#     it('this test should find a word by id') do
-#       new_word = Word.new({:name => "Consistency", :id => nil})
-#       new_word.save()
-#       new_word2 = Word.new({:name => "Monotony", :id => nil})
-#       new_word2.save()
-#       expect(Word.find(new_word.id)).to(eq(new_word))
-#     end
-#   end
+  describe('.find') do
+    it('this test should find a word by id') do
+      new_word = Word.new({:name => "Consistency", :id => nil})
+      new_word.save()
+      new_word2 = Word.new({:name => "Monotony", :id => nil})
+      new_word2.save()
+      expect(Word.find(new_word.id)).to(eq(new_word))
+    end
+  end
 
-#   describe('.search') do
-#     it("returns an array of words that have a name that includes the search term regardless of case") do
-#       new_word = Word.new({:name => "Consistency", :id => nil})
-#       new_word.save()
-#       new_word2 = Word.new({:name => "Monotony", :id => nil})
-#       new_word2.save()
-#       new_word3 = Word.new({:name => "Arbitrary", :id => nil})
-#       new_word3.save()
-#       new_word4 = Word.new({:name => "Thurough Consistency", :id => nil})
-#       new_word4.save()
-#       expect(Word.search("consistency")).to(eq([[new_word], [new_word4]]))
-#     end
-#   end
+  describe('.search') do
+    it("returns an array of words that have a name that includes the search term regardless of case") do
+      new_word = Word.new({:name => "Consistency", :id => nil})
+      new_word.save()
+      new_word2 = Word.new({:name => "Monotony", :id => nil})
+      new_word2.save()
+      new_word3 = Word.new({:name => "Arbitrary", :id => nil})
+      new_word3.save()
+      new_word4 = Word.new({:name => "Thurough Consistency", :id => nil})
+      new_word4.save()
+      expect(Word.search("consistency")).to(eq([[new_word], [new_word4]]))
+    end
+  end
 
-#   describe('#update') do
-#     it('updates a word based on id') do
-#       new_word = Word.new({:name => "Word", :id => nil})
-#       new_word.save()
-#       new_word.update("Conceive")
-#       expect(new_word.name).to(eq("Conceive")) 
-#     end
-#   end
+  describe('#update') do
+    it('updates a word based on id') do
+      new_word = Word.new({:name => "Word", :id => nil})
+      new_word.save()
+      new_word.update("Conceive")
+      expect(new_word.name).to(eq("Conceive")) 
+    end
+  end
 
-#   describe('#save') do
-#     it('updates the dictionary with a new word') do
-#       new_word = Word.new({:name => "Consistency", :id => nil})
-#       new_word.save()
-#       new_word2 = Word.new({
-#         :name => "Monotony",
-#         :id => nil
-#       })
-#       new_word2.save()
-#       expect(Word.all).to(eq([new_word, new_word2]))
-#     end
-#   end
+  describe('#save') do
+    it('updates the dictionary with a new word') do
+      new_word = Word.new({:name => "Consistency", :id => nil})
+      new_word.save()
+      new_word2 = Word.new({
+        :name => "Monotony",
+        :id => nil
+      })
+      new_word2.save()
+      expect(Word.all).to(eq([new_word, new_word2]))
+    end
+  end
 
-#   describe('#==') do
-#     it('checks for the same word if it has the same attributes as another word') do
-#       new_word = Word.new({:name => "Consistency", :id => nil})
-#       new_word.save()
-#       new_word2 = Word.new({
-#         :name => "Consistency",
-#         :id => nil
-#       })
-#       new_word2.save()
-#       expect(new_word).to(eq(new_word2))
-#     end
-#   end
+  describe('#==') do
+    it('checks for the same word if it has the same attributes as another word') do
+      new_word = Word.new({:name => "Consistency", :id => nil})
+      new_word.save()
+      new_word2 = Word.new({
+        :name => "Consistency",
+        :id => nil
+      })
+      new_word2.save()
+      expect(new_word).to(eq(new_word2))
+    end
+  end
 
-#   describe('#delete') do
-#     it('deletes a word by id') do
-#       new_word = Word.new({:name => "Consistency", :id => nil})
-#       new_word.save()
-#       new_word2 = Word.new({:name => "Monotony", :id => nil})
-#       new_word2.save()
-#       new_word.delete()
-#       expect(Word.all).to(eq([new_word2]))
-#     end
-#   end
-# end
+  describe('#delete') do
+    it('deletes a word by id') do
+      new_word = Word.new({:name => "Consistency", :id => nil})
+      new_word.save()
+      new_word2 = Word.new({:name => "Monotony", :id => nil})
+      new_word2.save()
+      new_word.delete()
+      expect(Word.all).to(eq([new_word2]))
+    end
+  end
+end
