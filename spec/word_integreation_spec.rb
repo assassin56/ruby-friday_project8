afce951153ad74ca9c198f622340763db405018c
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-26 20:18:06 -0700

    add two new tests for integration specs. Second has failed.

--- spec/word_integreation_spec.rb
+++ spec/word_integreation_spec.rb
@@ -1,17 +1,25 @@
-# integration tests
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
 
-# require('capybara/rspec')
-# require('./app')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
+describe('creates a word path', {:type => :feature}) do
+  it('creates a word and then goes to the words page') do
+    visit('/')
+    click_on('Add a new word!')
+    fill_in('name', :with => 'Consistency')
+    click_on('Go!')
+    expect(page).to have_content('Consistency')
+  end
+end
 
-# describe('creates a dictionary path', {:type => :feature}) do
-#   it('creates a dictionary and then goes to the dictionary page') do
-#     visit('/dictionary')
-#     save_and_open_page
-#     click_on('Add a new word!')
-#     fill_in('name', :with => 'Consistency')
-#     click_on('Go!')
-#     expect(page).to have_content('Consistency')
-#   end
-# end
+describe('creates a definition path', {:type => :feature}) do
+  it('creates a definition and goes to the definition page') do
+    word = Word.new({:name => 'Gold', :id => nil})
+    word.save()
+    visit("/words/#{word.id}")
+    fill_in('definition', :with => 'A form of currency')
+    click_on('Add definition')
+    expect(page).to have_content(false)
+  end
+end
